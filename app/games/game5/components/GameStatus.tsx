54e0b8472f2cf41774702fd1f539a7cdae945407
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2025-04-14 18:57:06 +0530

    Add Move
    Add move path visualization and valid move indicators for user assistance.

--- app/games/game5/components/GameStatus.tsx
+++ app/games/game5/components/GameStatus.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import { FC } from "react";
 
 interface GameStatusProps {
@@ -24,12 +24,12 @@ const GameStatus: FC<GameStatusProps> = ({ status, timeTaken }) => {
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-        <h2 className="text-2xl  text-black mb-4">
+        <h2 className="text-2xl text-black mb-4">
           {status === "win" ? "Victory!" : "Game Over"}
         </h2>
-        <p  className=" text-black">Time Taken: {formatTime(timeTaken)}</p>
+        <p className="text-black">Time Taken: {formatTime(timeTaken)}</p>
         <button
           className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           onClick={() => window.location.reload()}
