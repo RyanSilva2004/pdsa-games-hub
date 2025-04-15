82a1d6b368a8702402deb832f41abd2abaa293a9
difelk <dilshanfernando32@gmail.com>
2025-04-15 10:57:12 +0530

    db configuration issues fixed

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,4 +12,4 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 
-export { app  , firestore};
+export { app, firestore };
