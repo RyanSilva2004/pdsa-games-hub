93f4293c20d44239110b69eb31f544c364e0ce1e
Ilmee De Silva <ilmee.desilva@novigi.com.au>
2025-04-19 18:29:47 +0530

    db collection name changed

--- games/ticTacToe/utils/dbLogger.tsx
+++ games/ticTacToe/utils/dbLogger.tsx
@@ -8,7 +8,7 @@ export async function savePlayerWinResult(data: any) {
         board: JSON.stringify(data.board), 
         playerMoves: JSON.stringify(data.playerMoves), 
       };
-    await addDoc(collection(db, "playerWins"), resultToSave);
+    await addDoc(collection(db, "TTT_playerWins"), resultToSave);
     console.log("Player win result saved successfully:", resultToSave);
   } catch (error) {
     console.error("Error saving player win result:", error);
@@ -21,7 +21,7 @@ export async function saveGameWithComputerTiming(data: any) {
             ...data,
             board: JSON.stringify(data.board), 
           };
-        await addDoc(collection(db, "computerGameResults"), resultToSave);
+        await addDoc(collection(db, "TTT_computerGameResults"), resultToSave);
         console.log("Game result saved with computer timing successfully:", resultToSave);
       } catch (error) {
         console.error("Error saving game result:", error);
