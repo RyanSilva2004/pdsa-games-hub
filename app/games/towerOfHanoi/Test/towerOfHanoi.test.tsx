567738ffdf9e7510229ea8a913dbc9ccbc5939e9
RoshanChampika1 <126223467+RoshanChampika1@users.noreply.github.com>
2025-04-27 15:45:19 +0530

    Added two solution algorithms

--- app/games/towerOfHanoi/Test/towerOfHanoi.test.tsx
+++ app/games/towerOfHanoi/Test/towerOfHanoi.test.tsx
@@ -33,7 +33,7 @@ describe("TowerOfHanoi", () => {
     fireEvent.change(inputElement, { target: { value: "John Doe" } });
     fireEvent.click(startButton);
 
-    jest.advanceTimersByTime(300000); // Advance time by 5 minutes
+    jest.advanceTimersByTime(300000); 
 
     await waitFor(() => expect(screen.getByText("Game over! Time's up!")).toBeInTheDocument());
   });
@@ -41,21 +41,17 @@ describe("TowerOfHanoi", () => {
   it("should save the game result to the database when the player wins", async () => {
     render(<TowerOfHanoi />);
 
-    // Mock the axios POST request to save the game result
+    
     axios.post.mockResolvedValue({ data: { success: true } });
 
-    // Simulate the game play (you may need to simulate clicks on pegs to solve the puzzle)
-    // For simplicity, assuming game logic will result in a win
+    
     fireEvent.click(screen.getByRole('button', { name: /start game/i, disabled: false }));
-    jest.advanceTimersByTime(5000);  // Advance time slightly to simulate game progression
-
-    // Simulate clicking "Play Again" after winning
+    jest.advanceTimersByTime(5000);  
     fireEvent.click(screen.getByText("Play Again"));
 
-    // Check if the axios POST request was called
     await waitFor(() => expect(axios.post).toHaveBeenCalledWith("/api/towerOfHanoi", {
       playerName: "John Doe",
-      moves: expect.any(Number),  // You can further mock the moves count here if needed
+      moves: expect.any(Number),  
       timeTaken: expect.any(Number),
     }));
 
@@ -70,22 +66,21 @@ describe("TowerOfHanoi", () => {
 
     expect(screen.getByText("How to Play")).toBeInTheDocument();
 
-    // Using a more flexible regex matcher for the text to account for HTML breaks
+    
     expect(screen.getByText(/Move all disks from peg A to peg C/i)).toBeInTheDocument();
   });
 
   it("should reset the game when the 'Restart' button is clicked", () => {
     render(<TowerOfHanoi />);
 
-    // Start the game by filling out the name and clicking Start Game
+
     const inputElement = screen.getByPlaceholderText("Your name");
     fireEvent.change(inputElement, { target: { value: "John Doe" } });
     
-    // Be specific about which button to click to start the game
+    
     const startButton = screen.getByRole('button', { name: /start game/i, disabled: false });
     fireEvent.click(startButton);
 
-    // Simulate restarting the game
     const restartButton = screen.getByText("Restart");
     fireEvent.click(restartButton);
 
